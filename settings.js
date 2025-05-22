a9de82340862b9cbdf596efe1e46600df08770a1
sammieboi321 <abolanlefasuyi@gmail.com>
2025-05-23 00:46:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,13 +7,13 @@
 const fs = require('fs')
 const chalk = require('chalk')
 //contact details
-global.ownernomer = "255756530143"
-global.ownername = "HANSTZ"
+global.ownernomer = "2349074614697"
+global.ownername = "SAMMIE"
 global.ytname = "HANSTZ-TECH"
 global.socialm = "GitHub: Mrhanstz"
 global.location = "AFRICA, TANZANIA, DODOMA"
-global.ownernumber = '255756530143'  //creator number
-global.ownername = 'HANSTZ' //owner name
+global.ownernumber = '2349074614697'  //creator number
+global.ownername = 'SAMMIE' //owner name
 global.botname = 'HANS-XMD' //name of the bot
 //sticker details
 global.packname = 'Sticker By'
